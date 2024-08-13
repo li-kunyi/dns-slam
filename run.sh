07c9be57d7644ae61833d628b00e6d95158bd81c
Li-Kunyi <likunyi0823@gmail.com>
2024-08-13 21:04:03 +0000

    code release

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ python run.py configs/replica/room_0.yaml
 # python eval_ate.py configs/replica/room_0.yaml
 # python eval_ate.py configs/replica/office_0.yaml
 
-# python run.py configs/scannet/scene0000.yaml
+python run.py configs/scannet/scene0000.yaml
 # python run.py configs/scannet/scene0059.yaml
 # python run.py configs/scannet/scene0106.yaml
 # python eval_ate.py configs/scannet/scene0000.yaml
